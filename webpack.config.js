40173f120470293fd7720aaaea7f2d71d5f944ab
wekbor <364102504@qq.com>
2020-08-10 21:07:03 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin( {
+        title: '我要写代码了',
+        template: 'src/assets/index.html'
+      }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
